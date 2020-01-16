1e6bad19fdac3fde1383c00d33a63e179b61beda
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-01-16 19:02:33 +0530

    adding json

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,20 @@
+from typing import Any
+
 from django.shortcuts import render
+import json
+import requests
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{})
+
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=28D23F92-AB8C-44FD-94E5-B0A2EC8E4A86")
+
+    try:
+        api=json.loads(api_request.text)
+    except Exception as e :
+        api = "Error...."
+
+    return render(request, 'home.html',{'api': api})
 
 def about(request):
     return render(request, 'about.html',{})
